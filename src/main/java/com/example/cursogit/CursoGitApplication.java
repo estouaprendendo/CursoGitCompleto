6eca368a5dfca123c581badd6cac80b7e2621866
Estou Aprendendo <estouaprendendo@gmail.com>
2018-11-29 15:14:07 -0200

    Criação do site

---
+++ src/main/java/com/example/cursogit/CursoGitApplication.java
@@ -0,0 +1,12 @@
+package com.example.cursogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursoGitApplication.class, args);
+    }
+}
